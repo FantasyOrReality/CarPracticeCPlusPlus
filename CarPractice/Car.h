9335f598b41ac5c85b7ffc433171c3ff736c9bd0
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2022-09-21 15:22:44 +0100

    Completed Car output practice

---
+++ CarPractice/Car.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+class Car
+{
+public:
+	std::string Make;
+	std::string Model;
+	int Doors;
+	float Mileage;
+	float MPG;
+	float Petrol;
+};
+
--- CarPractice/CarPractice.cpp
+++ CarPractice/CarPractice.cpp
@@ -2,9 +2,50 @@
 //
 
 #include <iostream>
+#include "Car.h"
+
+void PrintCars(Car CarStats)
+{
+	std::cout << "The car's make is " << CarStats.Make << std::endl
+		<< ". The car's model is " << CarStats.Model << std::endl
+		<< ". The car has " << CarStats.Doors << " doors." << std::endl
+		<< " The car's mileage is " << CarStats.Mileage << std::endl
+		<< ". The car's MPG is " << CarStats.MPG << std::endl
+		<< ". The car has " << CarStats.Petrol << " gallons of petrol in it." << std::endl
+		<< std::endl;
+}
 
 int main()
 {
-   
+	Car McQueen;
+	Car Mater;
+	Car Doc;
+
+	McQueen.Make = "Chevrolet";
+	McQueen.Model = "Corvette C6";
+	McQueen.Doors = 2;
+	McQueen.Mileage = 100000.0;
+	McQueen.MPG = 100.0;
+	McQueen.Petrol = 100.0;
+
+	Mater.Make = "International Harvest";
+	Mater.Model = "Tow Truck";
+	Mater.Doors = 2;
+	Mater.Mileage = 500.0;
+	Mater.MPG = 50.0;
+	Mater.Petrol = 50.0;
+
+	Doc.Make = "Hudson";
+	Doc.Model = "Hornet";
+	Doc.Doors = 4;
+	Doc.Mileage = 500000.0;
+	Doc.MPG = 200.0;
+	Doc.Petrol = 200.0;
+
+	PrintCars(McQueen);
+	PrintCars(Mater);
+	PrintCars(Doc);
+
+
 }
 
